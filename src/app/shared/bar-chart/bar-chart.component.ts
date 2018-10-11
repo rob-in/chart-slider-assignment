5f4547084864c0fcc0fd74b3155e3e15e42bbf85
Robin Chauhan <rchauhan@clincase.com>
2018-10-11 12:02:27 +0200

    Refactored Bar chart component a bit

--- src/app/shared/bar-chart/bar-chart.component.ts
+++ src/app/shared/bar-chart/bar-chart.component.ts
@@ -41,6 +41,7 @@ export class BarChartComponent implements OnInit, OnChanges {
 
   createChart() {
     const element = this.chartContainer.nativeElement;
+    const { colors } = this._config.chartConfig;
     this.width = element.offsetWidth - this.margin.left - this.margin.right;
     this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
     const svg = d3
@@ -49,17 +50,17 @@ export class BarChartComponent implements OnInit, OnChanges {
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
 
-    // chart plot area
+    // generate chart plot area
     this.chart = svg
       .append('g')
       .attr('class', 'bars')
       .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
-    // define X & Y domains
+    // define X & Y domains for the chart
     const xDomain = this.data.map(d => d[0]);
     const yDomain = [0, d3.max(this.data, d => d[1])];
 
-    // create scales
+    // creating scales
     this.xScale = d3
       .scaleBand()
       .padding(0.1)
@@ -70,13 +71,13 @@ export class BarChartComponent implements OnInit, OnChanges {
       .domain(yDomain)
       .range([this.height, 0]);
 
-    // bar colors
+    // settingg bar colors from the cionfig
     this.colors = d3
       .scaleLinear()
       .domain([0, this.data.length])
-      .range(<any[]>['red', 'blue']);
+      .range(<any[]>[colors.active, colors.inactive]);
 
-    // x & y axis
+    // defining x & y axis
     this.xAxis = svg
       .append('g')
       .attr('class', 'axis axis-x')
@@ -90,6 +91,7 @@ export class BarChartComponent implements OnInit, OnChanges {
   }
 
   updateChart() {
+    const { colors } = this._config.chartConfig;
     // update scales & axis
     this.xScale.domain(this.data.map(d => d[0]));
     this.yScale.domain([0, d3.max(this.data, d => d[1])]);
@@ -110,7 +112,7 @@ export class BarChartComponent implements OnInit, OnChanges {
       .attr('y', (d: any) => this.yScale(d[1]))
       .attr('width', (d: any) => this.xScale.bandwidth())
       .attr('height', (d: any) => this.height - this.yScale(d[1]))
-      .style('fill', (d: any, i: any) => (d[2] ? '#00b7a1' : 'grey'));
+      .style('fill', (d: any, i: any) => (d[2] ? colors.active : colors.inactive));
 
     // add new bars
     update
@@ -121,7 +123,7 @@ export class BarChartComponent implements OnInit, OnChanges {
       .attr('y', (d: any) => this.yScale(0))
       .attr('width', this.xScale.bandwidth())
       .attr('height', 0)
-      .style('fill', (d: any, i: any) => (d[2] ? '#00b7a1' : 'grey'))
+      .style('fill', (d: any, i: any) => (d[2] ? colors.active : colors.inactive))
       .transition()
       .delay((d: any, i: any) => i * 10)
       .attr('y', (d: any) => this.yScale(d[1]))
